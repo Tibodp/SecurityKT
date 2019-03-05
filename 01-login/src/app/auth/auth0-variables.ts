83e699e0dd2cce2d62dc230c44c2436cd89594c1
Tibo Depovere <tibo.depovere@gmail.com>
2019-03-05 12:27:14 +0100

    eerste commit
    opdracht security

---
+++ 01-login/src/app/auth/auth0-variables.ts
@@ -0,0 +1,14 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+  apiUrl: String;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'KJwgp9nijauTpsdYx67wVM0UcNqzKR9b',
+  domain: 'karentibo.eu.auth0.com',
+  callbackURL: 'http://localhost:3000/callback',
+  apiUrl: 'https://securitykt.azurewebsites.net/api'
+
+};
