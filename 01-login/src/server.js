90f2302dcfd33a9a315851d9fab9c2833ad301c1
Tibo Depovere <tibo.depovere@gmail.com>
2019-03-05 16:30:45 +0100

    day finished

--- 01-login/src/server.js
+++ 01-login/src/server.js
@@ -44,21 +44,6 @@ const server = app.listen(port, hostname, () => {
 });
 
 //test
-//var request = require("request");
-
-//var options = { method: 'POST',
-  //url: 'https://login0.local.dev.auth0.com/oauth/token',
-  //headers: { 'content-type': 'application/json' },
-  //body: '{"client_id":"mznURxpeLh3xI1AxqZiIa10Fu2ycTSZ8","client_secret":"T5pzHeEj0nQh_Tvt6o3o57Vfm5ijImP6K5i5ajBS_6x27uqPewnFQJO-JichNNvP","audience":"https://securitykt.azurewebsites.net/api","grant_type":"client_credentials"}' };
-
-//request(options, function (error, response, body) {
-  //if (error) throw new Error(error);
-
-  //console.log(body);
-//});
-//test
-//var request = require("request");
-
 //var options = { method: 'GET',
   //url: 'http://path_to_your_api/',
   //headers: { authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlJFRkJPVVJHUXpBeE0wUXhNek14T1VJek1ESkZSVEJHUmtWRE4wUkZRelF3T1VWRlF6azFOUSJ9.eyJpc3MiOiJodHRwczovL2thcmVudGliby5ldS5hdXRoMC5jb20vIiwic3ViIjoibXpuVVJ4cGVMaDN4STFBeHFaaUlhMTBGdTJ5Y1RTWjhAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc2VjdXJpdHlrdC5henVyZXdlYnNpdGVzLm5ldC9hcGkiLCJpYXQiOjE1NTE3ODIwMTksImV4cCI6MTU1MTg2ODQxOSwiYXpwIjoibXpuVVJ4cGVMaDN4STFBeHFaaUlhMTBGdTJ5Y1RTWjgiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.HV851LYgSQ7o93eNiluerovMCbrH3_zofOIDniROiVVBmzzqTX0KO2U5eqVQoilVPvv-Wapj3c5m3pnP1ZgngiZh84lP-oXIpWElxAukD7ZDmMAN66FmRwJBoK1ZW_0RR6w7WnXTlHolgmWelrymBpKa55HYFyDih_XNlhhZqnLZR2fC3PVPCQBEh0HXen0X-I1EC0DkcFtsnNbWfrpsBtTHQJhJHDrMGK-b97ymL1TWhuTAiXLm8j3nQ7Y8_GiFiO4onJbB09hC5kboAUBphswrCA8WSHJhNpnXUXOPgthH279jsLxis8lDjr3CcTfSia8uK_hJtTEhkNC3uE8Y3A' } };
