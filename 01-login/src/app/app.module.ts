7cdeaba84cd529b7bd74b3e4688c715660e9d8a7
Tibo Depovere <tibo.depovere@gmail.com>
2019-03-15 11:29:05 +0100

    api stapke vejer

--- 01-login/src/app/app.module.ts
+++ 01-login/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CookieService, CookieOptions } from 'angular2-cookie/core';
 
 import { ROUTES } from './app.routes';
 
@@ -29,9 +31,10 @@ import { NamesService } from "./names.service";
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [AuthService, NamesService],
+  providers: [AuthService, NamesService, CookieService, {provide: CookieOptions, useValue: {}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
