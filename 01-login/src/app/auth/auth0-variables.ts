72f93ed72d307ce9f5fb8d293b444481311a8ee6
Tibo Depovere <tibo.depovere@gmail.com>
2019-03-15 09:03:02 +0100

    url

--- 01-login/src/app/auth/auth0-variables.ts
+++ 01-login/src/app/auth/auth0-variables.ts
@@ -8,7 +8,7 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'KJwgp9nijauTpsdYx67wVM0UcNqzKR9b',
   domain: 'karentibo.eu.auth0.com',
-  callbackURL: 'http://localhost:3000/callback',
+  callbackURL: 'https://securitykt.azurewebsites.net/callback',
   // **Student code change starts**
   apiUrl: 'https://securitykt.azurewebsites.net/api'
   // **Student code change ends**
